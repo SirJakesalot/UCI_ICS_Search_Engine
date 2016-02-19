385de64ef22ec8e8050961afc1806ca63e452120
Vinh Vu <kroomey@gmail.com>
2016-02-19 00:29:16 -0800

    Add comments and "encapsulation"

--- Indexer.py
+++ Indexer.py
@@ -6,7 +6,6 @@ Jennifer Chew       53649288
 Vinh Vu             21775557
 
 @Created: 02/18/16
-
 @Source: Indexer.py
 """
 
@@ -22,10 +21,10 @@ class Indexer():
     terms and documents which will be later used to provide a functional search engine.
 
     Attributes:
-        doc_id: The current available unused document ID. Also represents the amount 
-            of unique documents found so far (doc_id - 1).
-        term_id: The current available unused term ID. Also represents the amount of 
-            unique terms found so far (term_id - 1).
+        __doc_id: The current available unused document ID. Also represents the amount 
+            of unique documents found so far (__doc_id - 1).
+        __term_id: The current available unused term ID. Also represents the amount of 
+            unique terms found so far (__term_id - 1).
         doc_id_lookup: Dictionary mapping document URLs to their document ID {URL : doc_id}.
         term_id_lookup: Dictionary mapping terms to their term ID {term : term_id}.
         indexer: Dictionary mapping term IDs to their dictionary of document IDs to the 
@@ -33,9 +32,9 @@ class Indexer():
     """
 
     def __init__(self):
-        """Inits term and document ID to 1. The lookup datastructures to dictionaries."""
-        self.doc_id = 1
-        self.term_id = 1
+        """Inits term and document ID to 1 and the lookup datastructures to dictionaries."""
+        self.__doc_id = 1
+        self.__term_id = 1
         self.doc_id_lookup = dict()
         self.term_id_lookup = dict()
         self.indexer = dict()
@@ -51,21 +50,21 @@ class Indexer():
         """
         with open(file_name, 'r') as f:
             # First line holds the URL of the page
-            url = f.readline()
-            print "url", url
-            self.doc_id_lookup[url] = self.doc_id
+            url = f.readline().strip()
+            self.doc_id_lookup[url] = self.__doc_id
             
             lines = f.readlines()
             for line in lines:
-                for word in line.split(" "):
-                    s = re.sub('[^0-9a-zA-z]+', '', word)
-                    if len(s) != 0:
-                        if s not in self.term_id_lookup:
-                            self.term_id_lookup[s] = self.term_id
-                            self.indexer[self.term_id] = defaultdict(lambda: 0)
-                            self.term_id += 1
-                        self.indexer[self.term_id_lookup[s]][self.doc_id] += 1
-            self.doc_id += 1
+                for word in re.sub("[^0-9a-zA-Z]+", " ", line).split(" "):
+                    word = word.lower().strip()
+                    if len(word) != 0:
+                        if word not in self.term_id_lookup:
+                            self.term_id_lookup[word] = self.__term_id
+                            self.indexer[self.__term_id] = defaultdict(lambda: 0)
+                            self.__term_id += 1
+                        self.indexer[self.term_id_lookup[word]][self.__doc_id] += 1
+            self.__doc_id += 1
+
 
     def handleDir(self, dirname):
         """Opens the file/directory for processessing.
@@ -77,29 +76,83 @@ class Indexer():
             dirname: The name of the directory to be processed.
         """
         for dirpath, directories, files in os.walk(dirname):
+            print dirpath, directories, files
             for f in files:
+                print f
                 self.processPage(os.path.join(dirpath, f))
             for d in directories:
-                new_path = os.path.join(dirpath, d)
-                print "New path: " + new_path
-                self.handleDir(new_path)
+                print d
+                self.handleDir(os.path.join(dirpath, d))
+
+    #
+    # Getter functions.
+    #
+    def get_term_count(self):
+        """Returns the amount of unique terms found."""
+        return self.__term_id - 1
+
+    def get_doc_count(self):
+        """Returns the amount of unique documents found."""
+        return self.__doc_id - 1
 
     def get_inverse_term_lookup(self):
-        """Returns an inverse dictionary of term_id_lookup: term_id -> term."""
+        """Returns an inverse dictionary of term_id_lookup {term_id : term}."""
         return {v : k for k,v in self.term_id_lookup.items()}
 
     def get_inverse_doc_lookup(self):
-        """Returns an inverse dictionary of doc_id_lookup: doc_id -> doc."""
+        """Returns an inverse dictionary of doc_id_lookup {doc_id : doc}"""
         return {v : k for k,v in self.doc_id_lookup.items()}       
+
+
+    #
+    # Printing functions.
+    #
+    def print_term_lookup(self, by_id=True):
+        """Prints the key, value pairs of term_id_lookup.
+
+        Args:
+            by_id: Boolean indicating whether to sort by ID, otherwise by name.
+        """
+        for k,v in sorted(self.term_id_lookup.items(), key=lambda x: x[by_id]):
+            print "({} : {})".format(k,v)
+
+    def print_doc_lookup(self, by_id=True):
+        """Prints the key, value pairs of doc_id_lookup.
+
+        Args:
+            by_id: Boolean indicating whether to sort by ID, otherwise by name.
+        """
+        for k,v in sorted(self.doc_id_lookup.items(), key=lambda x: x[by_id]):
+            print "({} : {})".format(k,v)
+
+    def print_inverse_term_lookup(self, by_id=True):
+        """Prints the inverse key, value pairs of term_id_lookup.
+        
+        Args:
+            by_id: Boolean indicating whether to sort by ID, otherwise by name.
+        """
+        for k,v in sorted(self.get_inverse_term_lookup().items(), key=lambda x: x[not by_id]):
+            print "({} : {})".format(k,v)
+
+    def print_inverse_doc_lookup(self, by_id=True):
+        """Prints the inverse key, value pairs of doc_id_lookup.
+        
+        Args:
+            by_id: Boolean indicating whether to sort by ID, otherwise by name.
+        """
+        for k,v in sorted(self.get_inverse_doc_lookup().items(), key=lambda x: x[not by_id]):
+            print "({} : {})".format(k,v)
 
 
 if __name__ == "__main__":
     indexer = Indexer()
     indexer.handleDir("test");
     terms = indexer.term_id_lookup
-    for k,v in indexer.get_inverse_term_lookup().items():
-        print "key", k
-        print v
+
+    indexer.print_term_lookup()
+    #indexer.print_doc_lookup()
+    #indexer.print_inverse_term_lookup()
+    #indexer.print_inverse_doc_lookup()
 
     """
     for key,val in sorted(indexer.indexer.items()):
