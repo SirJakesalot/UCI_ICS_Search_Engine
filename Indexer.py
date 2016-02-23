e9198a1b3a2f91fd7186109d69b3f3c959994aad
Vinh Vu <kroomey@gmail.com>
2016-02-23 11:01:45 -0800

    Add TF-IDF indexing.

--- Indexer.py
+++ Indexer.py
@@ -13,13 +13,16 @@
 import os
 import re
 import sys
+import copy
+import math
 import time
 from collections import defaultdict
 
-class Indexer():
+
+class Indexer:
     """Indexer class for the search engine index.
 
-    Provides all methods and datastructures to appropriately process, index and store 
+    Provides all methods and data structures to appropriately process, index and store
     terms and documents which will be later used to provide a functional search engine.
 
     Attributes:
@@ -29,22 +32,27 @@ class Indexer():
             unique terms found so far (__term_id - 1).
         doc_id_lookup: Dictionary mapping document URLs to their document ID {URL : doc_id}.
         term_id_lookup: Dictionary mapping terms to their term ID {term : term_id}.
+        doc_term_count: Dictionary mapping documents to their word count {doc_id : count}.
         indexer: Dictionary mapping term IDs to their dictionary of document IDs to the 
             frequency count within that same document {term_id : {doc_id : frequency}}.
+        tf_idf: Dictionary mapping term IDs to their dictionary of document IDs to the
+            TF-IDF (Term Frequency - Inverse Document Frequency) {term_id: {doc_id : tf-idf}}.
     """
 
     def __init__(self):
-        """Inits term and document ID to 1 and the lookup datastructures to dictionaries."""
+        """Initializes term and document ID to 1 and the lookup data structures to dictionaries."""
         self.__doc_id = 1
         self.__term_id = 1
         self.doc_id_lookup = dict()
         self.term_id_lookup = dict()
+        self.doc_term_count = dict()
         self.indexer = dict()
+        self.tf_idf = dict()
 
     #
     # Processing functions.
     #
-    def processPage(self, file_name):
+    def process_page(self, file_name):
         """Processes the page.
 
         Processes the page by stripping the read lines of whitespace and non-alphanumeric 
@@ -56,12 +64,15 @@ class Indexer():
         with open(file_name, 'r') as f:
             # First line holds the URL of the page
             url = f.readline().strip()
+            word_count = 0
             self.doc_id_lookup[url] = self.__doc_id
-            
+
             lines = f.readlines()
             for line in lines:
                 for word in re.sub("[^0-9a-zA-Z]+", " ", line).split(" "):
                     self.__add_word(word)
+                    word_count += 1
+            self.doc_term_count[self.__doc_id] = word_count
             self.__doc_id += 1
 
     def __add_word(self, word):
@@ -81,18 +92,18 @@ class Indexer():
                 self.__term_id += 1
             self.indexer[self.term_id_lookup[word]][self.__doc_id] += 1
 
-    def handleDir(self, dirname):
-        """Opens the file/directory for processessing.
+    def handle_dir(self, dirname):
+        """Opens the file/directory for processing.
 
         Opens the directory and lists all the files/directories for processing.
-        Recursively calls into any directories found, otherwise calls handleFile().
+        Recursively calls into any directories found, otherwise calls process_page().
 
         Args:
             dirname: The name of the directory to be processed.
         """
         for dirpath, directories, files in os.walk(dirname):
             for f in files:
-                self.processPage(os.path.join(dirpath, f))
+                self.process_page(os.path.join(dirpath, f))
 
     def save_indexer_to_files(self):
         """Writes the index data to a file.
@@ -101,24 +112,48 @@ class Indexer():
         file.
 
         Files written:
-            index.txt - Contains the indexer data
-            terms.txt - Contains the term to term_id data
-            docs.txt - Contains the doc to doc_id data
+            index.txt - Contains the indexer data.
+            tf_idf.txt - Contains the TF-IDF data.
+            terms.txt - Contains the term to term_id data.
+            docs.txt - Contains the doc to doc_id data.
 
         Args:
             file_name: The name of the file to be written to.
         """
         stdout = sys.stdout
         files = [("index.txt", self.print_indexer),
+                 ("tf_idf.txt", self.print_tf_idf),
+                 ("doc_lengths.txt", self.print_doc_lengths),
                  ("terms.txt", self.print_term_lookup),
                  ("docs.txt", self.print_doc_lookup)]
 
-        for name,func in files:
+        for name, func in files:
             with open(name, "w") as f:
                 sys.stdout = f
                 func()
 
         sys.stdout = stdout
+
+    def __calculate_tf_idf(self, term_freq, doc_freq, doc_id):
+        """Calculates the TF-IDF for the term.
+
+        Args:
+            term_freq: The frequency of the term.
+            doc_freq: The amount of documents the term appears in.
+            doc_id: The document ID.
+        """
+        return (1 + math.log(float(term_freq), 10)) * math.log(float(self.num_docs)/doc_freq, 10)
+
+    def create_tf_idf(self):
+        """Creates the TF-IDF indexer dictionary.
+
+        The dictionary copies its values from the indexer data structure and replaces the term
+        frequency value with the TF-IDF value.
+        """
+        self.tf_idf = copy.deepcopy(self.indexer)
+        for k, v in self.tf_idf.items():
+            for doc, tf in v.items():
+                self.tf_idf[k][doc] = self.__calculate_tf_idf(tf, len(v), doc)
 
 
     #
@@ -136,11 +171,11 @@ class Indexer():
 
     def get_inverse_term_lookup(self):
         """Returns an inverse dictionary of term_id_lookup {term_id : term}."""
-        return {v : k for k,v in self.term_id_lookup.items()}
+        return {v: k for k, v in self.term_id_lookup.items()}
 
     def get_inverse_doc_lookup(self):
         """Returns an inverse dictionary of doc_id_lookup {doc_id : doc}"""
-        return {v : k for k,v in self.doc_id_lookup.items()}       
+        return {v: k for k, v in self.doc_id_lookup.items()}
 
 
     #
@@ -152,8 +187,8 @@ class Indexer():
         Args:
             by_id: Boolean indicating whether to sort by ID, otherwise by name.
         """
-        for k,v in sorted(self.term_id_lookup.items(), key=lambda x: x[by_id]):
-            print "({} : {})".format(k,v)
+        for k, v in sorted(self.term_id_lookup.items(), key=lambda x: x[by_id]):
+            print "({} : {})".format(k, v)
 
     def print_doc_lookup(self, by_id=True):
         """Prints the key, value pairs of doc_id_lookup.
@@ -161,8 +196,8 @@ class Indexer():
         Args:
             by_id: Boolean indicating whether to sort by ID, otherwise by name.
         """
-        for k,v in sorted(self.doc_id_lookup.items(), key=lambda x: x[by_id]):
-            print "({} : {})".format(k,v)
+        for k, v in sorted(self.doc_id_lookup.items(), key=lambda x: x[by_id]):
+            print "({} : {})".format(k, v)
 
     def print_inverse_term_lookup(self, by_id=True):
         """Prints the inverse key, value pairs of term_id_lookup.
@@ -170,8 +205,8 @@ class Indexer():
         Args:
             by_id: Boolean indicating whether to sort by ID, otherwise by name.
         """
-        for k,v in sorted(self.get_inverse_term_lookup().items(), key=lambda x: x[not by_id]):
-            print "({} : {})".format(k,v)
+        for k, v in sorted(self.get_inverse_term_lookup().items(), key=lambda x: x[not by_id]):
+            print "({} : {})".format(k, v)
 
     def print_inverse_doc_lookup(self, by_id=True):
         """Prints the inverse key, value pairs of doc_id_lookup.
@@ -179,7 +214,7 @@ class Indexer():
         Args:
             by_id: Boolean indicating whether to sort by ID, otherwise by name.
         """
-        for k,v in sorted(self.get_inverse_doc_lookup().items(), key=lambda x: x[not by_id]):
+        for k, v in sorted(self.get_inverse_doc_lookup().items(), key=lambda x: x[not by_id]):
             print "({} : {})".format(k,v)
 
     def print_indexer(self, print_word=False):
@@ -189,20 +224,39 @@ class Indexer():
         has dictionary values which indicates its frequency in a specific document.
 
         Args:
-            with_word: Flag indicating if the user wants to print out the actual word along
-            with the term.
+            print_word: Flag indicating if the user wants to print out the actual word along
+                with the term.
         """
-        for k,v in self.indexer.items():
+        for k, v in self.indexer.items():
             term = self.get_inverse_term_lookup()[k] if print_word else ""
             print "({} {} : ({})".format(k, term, dict(v))
 
+    def print_tf_idf(self, print_word=False):
+        """Prints the key, value pairs of the indexer.
+
+        The format for the index is {term_id : {doc_id, tf-idf}}. In English, each term_id
+        has dictionary values which indicates its tf-idf value in a specific document.
+
+        Args:
+            print_word: Flag indicating if the user wants to print out the actual word along
+                with the term.
+        """
+        for k, v in self.tf_idf.items():
+            term = self.get_inverse_term_lookup()[k] if print_word else ""
+            print "({} {} : ({})".format(k, term, dict(v))
+
+    def print_doc_lengths(self):
+        """Prints the document ID, document length (word count) pairs."""
+        for k, v in self.doc_term_count.items():
+            print "({} : {})".format(k, v)
 
 if __name__ == "__main__":
     indexer = Indexer()
 
     # Time the indexer
     start = time.time()
-    indexer.handleDir("data");
+    indexer.handle_dir("data")
+    indexer.create_tf_idf()
     end = time.time()
 
     print "Time:", str(end - start), "seconds"
